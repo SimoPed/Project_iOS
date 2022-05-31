14999b7d5403d6d9eae72aadf067085c7113097b
SimoPed <103127889+SimoPed@users.noreply.github.com>
2022-05-31 10:02:12 +0200

    primo commit, speriamo sia la volta buona

---
+++ GameTrivia_iOS/GameTrivia_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameTrivia_iOS
+//
+//  Created by Simone Pedrini on 31/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
