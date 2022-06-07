70ce17b10f9981800a2ac1842938113961c0c88f
SimoPed <simone.pedrini12@gmail.com>
2022-06-07 12:07:48 +0200

    356 errori, ma a noi va bene lo stesso

--- GameTrivia_iOS/GameTrivia_iOS/QuestionsViewController.swift
+++ GameTrivia_iOS/GameTrivia_iOS/QuestionsViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import Foundation
 
 class QuestionsViewController: UIViewController {
+    
+    struct Music: Codable {
+        var question: String
+        var correct_answer: String
+        var incorrect_answers: [String]
+    }
     
     
     
     override func viewDidLoad() {
         super .viewDidLoad()
+        musicQuestions()
     }
     
-    let url = URL(string: "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&encode=base64")
+    
+    func musicQuestions(completionHandler: @escaping ([Music]) -> Void) {
+        let url = URL(string: "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy")!
+//        let data = try! Data(contentsOf: url)
+//        do {
+//            let decoder: JSONDecoder = JSONDecoder.init()
+//            let user: Music = try decoder.decode(Music.self, from: data)
+//
+//            print("Dio bastardo \(user.question)")
+//        } catch _ {
+//            print("Dio madonna")
+//        }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+              if let error = error {
+                print("Error with fetching films: \(error)")
+                return
+              }
+              
+              guard let httpResponse = response as? HTTPURLResponse,
+                    (200...299).contains(httpResponse.statusCode) else {
+                print("Error with the response, unexpected status code: \(response)")
+                return
+              }
+
+              if let data = data,
+                let musicSummary = try? JSONDecoder().decode(Music.self, from: data) {
+//                completionHandler(musicSummary.question ?? [])
+              }
+            })
+            task.resume()
+        
+    }
+    
 }
