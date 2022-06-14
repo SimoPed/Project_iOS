b068cce72202138e347f60919e0b2ddeee8206af
SimoPed <simone.pedrini12@gmail.com>
2022-06-14 12:41:27 +0200

    sium

--- GameTrivia_iOS/GameTrivia_iOS/QuestionsViewController.swift
+++ GameTrivia_iOS/GameTrivia_iOS/QuestionsViewController.swift
@@ -11,25 +11,40 @@ import Firebase
 
 class QuestionsViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var firstButtonAnswer: UIButton!
+    @IBOutlet weak var secondButtonAnswer: UIButton!
+    @IBOutlet weak var thirdButtonAnswer: UIButton!
+    @IBOutlet weak var fourthButtonAnswer: UIButton!
+    
     let database = Firestore.firestore()
     
     override func viewDidLoad() {
         super .viewDidLoad()
+        firstButtonAnswer.questionButton()
+        secondButtonAnswer.questionButton()
+        thirdButtonAnswer.questionButton()
+        fourthButtonAnswer.questionButton()
         musicQuestions()
     }
     
     
     func musicQuestions() {
         let readDoc = database.collection("music").getDocuments() {(querySnapshot, err) in
-            if let err = err {
-                print("Error")
-            } else {
+//            if let err = err {
+//                print("Error")
+//            } else {
 //                for document in querySnapshot!.documents {
 //                print("\(querySnapshot?.documents[0].documentID) => \(querySnapshot?.documents[0].data())")
 //                }
-                var dati = querySnapshot?.documents[0].data()
-                print(dati)
-            }
+            var dati = querySnapshot!.documents[0].data()
+//            print(dati["question"])
+            self.questionLabel.text = dati["question"] as! String
+            self.firstButtonAnswer.setTitle(dati["answerCorrect"] as! String, for: .normal)
+            self.secondButtonAnswer.setTitle(dati["answerOne"] as! String, for: .normal)
+            self.thirdButtonAnswer.setTitle(dati["answerTwo"] as! String, for: .normal)
+            self.fourthButtonAnswer.setTitle(dati["answerThree"] as! String, for: .normal)
+//            }
         }
     
     }
