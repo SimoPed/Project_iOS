1d4a9735bd302db5ec4df62f12bb28f99b934c50
SimoPed <simone.pedrini12@gmail.com>
2022-06-21 09:48:27 +0200

    ciao

--- GameTrivia_iOS/GameTrivia_iOS/QuestionsViewController.swift
+++ GameTrivia_iOS/GameTrivia_iOS/QuestionsViewController.swift
@@ -17,6 +17,26 @@ class QuestionsViewController: UIViewController {
     @IBOutlet weak var thirdButtonAnswer: UIButton!
     @IBOutlet weak var fourthButtonAnswer: UIButton!
     
+
+    @IBAction func correctAnswerTapped(_ sender: Any) {
+        firstButtonAnswer.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func wrongOneAnswerTapped(_ sender: Any) {
+        secondButtonAnswer.backgroundColor = UIColor.red
+        firstButtonAnswer.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func wrongTwoAnswerTapped(_ sender: Any) {
+        thirdButtonAnswer.backgroundColor = UIColor.red
+        firstButtonAnswer.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func wrongThreeAnswerTapped(_ sender: Any) {
+        fourthButtonAnswer.backgroundColor = UIColor.red
+        firstButtonAnswer.backgroundColor = UIColor.green
+    }
+    
     let database = Firestore.firestore()
     
     override func viewDidLoad() {
@@ -31,20 +51,13 @@ class QuestionsViewController: UIViewController {
     
     func musicQuestions() {
         let readDoc = database.collection("music").getDocuments() {(querySnapshot, err) in
-//            if let err = err {
-//                print("Error")
-//            } else {
-//                for document in querySnapshot!.documents {
-//                print("\(querySnapshot?.documents[0].documentID) => \(querySnapshot?.documents[0].data())")
-//                }
             var dati = querySnapshot!.documents[0].data()
-//            print(dati["question"])
             self.questionLabel.text = dati["question"] as! String
             self.firstButtonAnswer.setTitle(dati["answerCorrect"] as! String, for: .normal)
             self.secondButtonAnswer.setTitle(dati["answerOne"] as! String, for: .normal)
             self.thirdButtonAnswer.setTitle(dati["answerTwo"] as! String, for: .normal)
             self.fourthButtonAnswer.setTitle(dati["answerThree"] as! String, for: .normal)
-//            }
+            
         }
     
     }
